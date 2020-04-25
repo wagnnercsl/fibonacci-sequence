56578df7bf4539638f35b93d5f1b0314cf1af6b4
Wagnner <wagnner.leivas@gmail.com>
2020-04-25 09:04:07 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const fibonacci = () => {
     let fibonacciSequence = [0,1], currentElement = 0;
     
-    for(let i=2; i<=350; i++){
+    for(let i=2;; i++){
         currentElement = fibonacciSequence[i] = fibonacciSequence[i-1] + fibonacciSequence[i-2];
 
         if(currentElement > 350) {
